13474930adbc3c73fbbe985df4e7c7ff49c34ab9
jingliang <jingliang.hu@dlr.de>
2021-06-19 23:57:51 +0200

    update test_run.py

--- experiments/test/test_run.py
+++ experiments/test/test_run.py
@@ -24,18 +24,18 @@ paraDict = {
         "training_patient": 20,
         "lr_rate": 0.1,
         "lr_rate_patient": 5,
-        "val_percent": 0.2,
+        "val_percent": 0.1,
         "save_best_model": 1,
         ### data loading parameters
         #"trainData": "lcz42", # training data could be the training data of LCZ42 data, or data of one of the cultural-10 city
-        "trainData": env_path+"/data/00010_23083_NewYork_Patch_32_LabPerc_70_bal.h5",
+        "trainData": env_path+"/data/train.h5",
         #"testData": "cul10",  # testing data could be all the data of the cultural-10 cities, or one of them.
-        "testData": env_path+"/data/00010_23083_NewYork_Patch_32_LabPerc_70_bal.h5",
+        "testData": env_path+"/data/test.h5",
         ### model name
         "backbone_model": 'unet',
         "exper_description": 'test_debug_run',
         }
-cudaNow = torch.device("cuda:2")
+cudaNow = torch.device("cuda:0")
 
 '''
 record experiment parameters
@@ -73,8 +73,9 @@ STEP THREE: Define a loss function and optimizer
 '''
 import torch.optim as optim
 import torch.nn as nn
-classWeight = torch.tensor([0,1/3,1/3,1/3]).to(cudaNow)
-criterion = nn.CrossEntropyLoss(weight=classWeight)
+# classWeight = torch.tensor([0,1/3,1/3,1/3]).to(cudaNow)
+# criterion = nn.CrossEntropyLoss(weight=classWeight)
+criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=paraDict["lr_rate"])
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=paraDict["lr_rate_patient"], threshold=0.0001)
 
